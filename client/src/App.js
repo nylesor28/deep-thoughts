df5b4194f2eb17ebe417f2e34aab1fa8f5528a77
nylesor28 <81473475+nylesor28@users.noreply.github.com>
2021-10-10 13:09:33 -0400

    set up react router

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,17 @@ import {
   ApolloProvider,
   createHttpLink,
 } from '@apollo/client';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -24,13 +29,22 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Router>
       <div className="flex-column justify-flex-start min-100-vh">
         <Header />
         <div className="container">
-          <Home />
+          <Switch>
+        <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/thought" component={SingleThought} />
+            <Route component = {NoMatch} />
+            </Switch>
         </div>
         <Footer />
       </div>
+      </Router>
     </ApolloProvider>
   );
 }
